e8b1561715097b817bb2f2842da193f6f8a79dc2
Haider Ali Punjabi <haiderali@cyberservices.com>
2017-01-18 20:17:57 +0530

    testing p5

--- js/projects.js
+++ js/projects.js
@@ -1,17 +1,48 @@
 var fileExt = {};
     fileExt[0]=".html";
-    fileExt[1]=".jpg";
-    fileExt[2]=".gif";
+var filenames = [];
 $.ajax({
     //This will retrieve the contents of the folder if the folder is configured as 'browsable'
     url: 'projects/',
     success: function (data) {
-       $("#fileNames").html('<ul>');
-       //List all png or jpg or gif file names in the page
-       $(data).find("a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + ")").each(function () {
-           var filename = this.href.replace(window.location.host, "").replace("file:///", "");
-           $("#fileNames").append( "<li>" + filename + "</li>");
+       $(data).find("a:contains(" + fileExt[0] + ")").each(function () {
+           var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+          filenames.push(filename)
+
        });
-       $("#fileNames").append('</ul>');
+       filter();
+       parsePages();
      }
   });
+
+function filter(){
+filenames.forEach(function(item, index){
+  if(item == "index.html"){
+    filenames.splice(index,1);
+  }
+});
+
+}
+
+function parsePages()
+{
+  filenames.forEach(function(item, index){
+    parsePage(item);
+  });
+}
+
+function parsePage(page)
+{
+  var pageURL = "projects/" + page;
+  var contextText;
+/*  jQuery(function($){
+  	$().load(pageURL + ' #project-background', function(result){console.log($().text())});
+  });*/
+  var content;
+$.get('pageURL' +  " #project-background", function(data){
+    content= data;
+});
+// Do something with content:
+alert(content);
+console.log(content);
+}
