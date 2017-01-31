28f0d0afb129be9d71ca9e128440f101320d345a
Haider Ali Punjabi <haiderali@cyberservices.com>
2017-01-31 21:10:48 +0530

    p5 Painting

--- p5/painting/particle.js
+++ p5/painting/particle.js
@@ -1,4 +1,4 @@
-function particle(x_, y_, magnitude_, angle_, mass_) 
+function particle(x_, y_, magnitude_, angle_, mass_)
 {
   this.x = x_;
   this.y = y_;
@@ -17,7 +17,7 @@ function particle(x_, y_, magnitude_, angle_, mass_)
     this.angle = angle_;
     this.mass = mass_;
   }
-  
+
   this.gravitate = function(Z) {
     var F, mX, mY, A;
     if( sq( this.x - Z.x ) + sq( this.y - Z.y ) != 0 ) {
@@ -25,13 +25,13 @@ function particle(x_, y_, magnitude_, angle_, mass_)
       mX = ( this.mass * this.x + Z.mass * Z.x ) / ( this.mass + Z.mass );
       mY = ( this.mass * this.y + Z.mass * Z.y ) / ( this.mass + Z.mass );
       A = findAngle( mX - this.x, mY - this.y );
-      
+
       mX = F * cos(A);
       mY = F * sin(A);
-      
+
       mX += this.magnitude * cos(this.angle);
       mY += this.magnitude * sin(this.angle);
-      
+
       this.magnitude = sqrt( sq(mX) + sq(mY) );
       this.angle = findAngle( mX, mY );
     }
@@ -44,34 +44,35 @@ function particle(x_, y_, magnitude_, angle_, mass_)
       mX = ( this.mass * this.x + Z.mass * Z.x ) / ( this.mass + Z.mass );
       mY = ( this.mass * this.y + Z.mass * Z.y ) / ( this.mass + Z.mass );
       A = findAngle( this.x - mX, this.y - mY );
-      
+
       mX = F * cos(A);
       mY = F * sin(A);
-      
+
       mX += this.magnitude * cos(this.angle);
       mY += this.magnitude * sin(this.angle);
-      
+
       this.magnitude = sqrt( sq(mX) + sq(mY) );
       this.angle = findAngle( mX, mY );
     }
   }
-  
+
   this.deteriorate = function() {
     this.magnitude *= 0.925;
   }
-  
+
   this.update = function() {
-    
+
     this.x += this.magnitude * cos(this.angle);
     this.y += this.magnitude * sin(this.angle);
-    
+
   }
-  
+
   this.display = function() {
-    strokeWeight(3);
-    line(this.px,this.py,this.x,this.y);
-    this.px = this.x;
-    this.py = this.y;
+    noStroke();
+    fill(colour[0], colour[1], colour[2], slider.value)
+    rect(this.x,this.y,5,5);
+  //  this.px = this.x;
+  //  this.py = this.y;
   }
 }
 
@@ -95,4 +96,3 @@ function findAngle(x, y) {
   }
   return theta;
 }
- 
--- p5/painting/sketch.js
+++ p5/painting/sketch.js
@@ -1,17 +1,17 @@
-var vScale = 16;
-var particles =[];
+var vScale = 8;
+var Z =[];
 var video;
 var slider;
-var noParticles = 50;
+var noParticles = 500;
 function setup()
 {
-  createCanvas(640,480);
+  createCanvas(640, 500);
   pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(window.width/vScale, window.height/vScale);
   for(var i = 0; i < noParticles; i++)
   {
-    particles[i] = new Particle(320,240);
+    Z[i] = new particle(random(window.width),random(window.height),0,0,1);
   }
   slider = createSlider(0,255, 127);
   background(255);
@@ -20,8 +20,16 @@ function setup()
 function draw()
 {
   video.loadPixels();
-for(var i = 0; i<noParticles; i++ )
-  {particles[i].update();
-  particles[i].show();
-}
+  var r;
+
+  for(var i = 0; i < Z.length; i++) {
+    Z[i].gravitate( new particle(floor(random(0,window.width)), floor(random(0,window.height)), 0, 0, 1 ) );
+    Z[i].update();
+    r = i/Z.length;
+    px = floor(Z[i].x / vScale);
+    py = floor(Z[i].y / vScale);
+    colour = video.get(px,py);
+    Z[i].display(colour);
+  }
+
 }
