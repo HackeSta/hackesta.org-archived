e8d4dc3759823a66309adb7d4a6ee2e8d0f9d221
haideralipunjabi <haiderali@cyberservices.com>
2017-04-13 19:46:48 +0530

    Improved /projects.html

--- js/projects.js
+++ js/projects.js
@@ -1,22 +1,42 @@
+  var langs = [];
 jQuery(document).ready(function($) {
   var url = $.url();
-  var lang = 'all'
 
-    if(url.param('lang') !== undefined && url.param('lang') !== '')
+  var all = true;
+  var query = '';
+    if(url.param('langs') !== undefined && url.param('langs') !== '')
     {
-      lang = url.param('lang')
+      query = url.param('langs');
+      $(query.split(',')).each(function(index) {
+        langs.push(this.toString());
+      });
+      all = false;
     }
-    $("#project-title").append(" - " + lang.toString());
+    $("#project-title").append(" - " + query.toString());
   $.getJSON("assets/projects.json", function(json){
      $(json.projects).reverse().each(function(index) {
-       if(lang == 'all') {
+       if(all) {
          $("#project-cards").append('<div id="'+this.id+'" class="card col"><a href="'+this.link+'"><img class="img-thumbnail" alt="Thumbnail" src="'+this.img+'"><h2 class="title card-ele" href="'+this.link+'">'+this.title+'</h2></a><h2 class="lang card-ele">'+this.client+'</h2><p class="description">'+this.description+'</p></div>');
        }
        else{
-         if(this.language == lang) $("#project-cards").append('<div id="'+this.id+'" class="card col"><a href="'+this.link+'"><img class="img-thumbnail" alt="Thumbnail" src="'+this.img+'"><h2 class="title card-ele" href="'+this.link+'">'+this.title+'</h2></a><h2 class="lang card-ele">'+this.client+'</h2><p class="description">'+this.description+'</p></div>');
+         //console.log(isinLangs(this.languages));
+         if(isinLangs(this.languages) === true){
+           $("#project-cards").append('<div id="'+this.id+'" class="card col"><a href="'+this.link+'"><img class="img-thumbnail" alt="Thumbnail" src="'+this.img+'"><h2 class="title card-ele" href="'+this.link+'">'+this.title+'</h2></a><h2 class="lang card-ele">'+this.client+'</h2><p class="description">'+this.description+'</p></div>');
        }
+     }
      });
   });
+
+
 });
 
+function isinLangs(lang){
+  var retVar = false;
+  $(lang.split(',')).each(function(index){
+    if(langs.indexOf(this.toString()) >= 0) {
+      retVar = true;
+    }
+  });
+  return retVar;
+}
 jQuery.fn.reverse = [].reverse;
