c6c016c3601521b28d0fafb05fbaaca383d53739
haideralipunjabi <haiderali@cyberservices.com>
2017-06-15 12:00:34 +0530

    Added GameofLife

---
+++ p5/gameoflife/libraries/cell.js
@@ -0,0 +1,50 @@
+function cell(i,j){
+  this.i = i;
+  this.j = j;
+  this.x = i * 50;
+  this.y = j * 50;
+}
+
+cell.prototype.show = function(){
+};
+cell.prototype.draw = function() {
+  stroke(3);
+  if(!this.dead)
+  {
+    fill(0,255,0);
+  }
+  else{
+    fill(255);
+  }
+  rect(this.x, this.y, w, w);
+};
+
+cell.prototype.process = function(){
+  var total = 0;
+  for (var xoff = -1; xoff <= 1; xoff++) {
+    var i = this.i + xoff;
+    if (i < 0 || i >= s) continue;
+
+    for (var yoff = -1; yoff <= 1; yoff++) {
+      var j = this.j + yoff;
+      if (j < 0 || j >= s) continue;
+
+      var neighbor = cells[i][j];
+      if (!neighbor.dead) {
+        total++;
+      }
+    }
+  }
+  if(!this.dead){
+    if(!(total === 2 || total === 3)){
+      this.dead = false;
+    }
+    if(total < 2) console.log("lonely");
+    if(total > 3) console.log("overcrowd");
+  }
+  else{
+    if(total === 3){
+      this.dead = false;
+    }
+  }
+};
---
+++ p5/gameoflife/sketch.js
@@ -0,0 +1,56 @@
+function make2DArray(cols, rows) {
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+
+var w = 500;
+var h = 500;
+var s = 10;
+var cells;
+function setup()
+{  
+  createCanvas(w,h);
+  cells = make2DArray(s,s);
+  for (var i = 0; i < s; i++) {
+    for (var j = 0; j < s; j++) {
+      cells[i][j] = new cell(i, j);
+    }
+  }
+    for (var i = 0; i < s; i++) {
+      for (var j = 0; j < s; j++) {
+        rand = random(1);
+        if(rand >= 0.5){
+          cells[i][j].dead = false;
+        }
+        else{
+          cells[i][j].dead = true;
+        }
+      }
+      
+    
+}
+    btn = createButton('Process');
+    btn.mousePressed(process);
+}
+
+function draw()
+{
+  background(255);
+  for (var i = 0; i < s; i++) {
+    for (var j = 0; j < s; j++) {
+      cells[i][j].draw();
+    }
+}
+}
+
+function process(){
+  for (var i = 0; i < s; i++) {
+    for (var j = 0; j < s; j++) {
+      cells[i][j].process();
+    }  
+}
+}
