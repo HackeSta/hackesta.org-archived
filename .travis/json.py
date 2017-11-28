0954b713c0a57b87b36dc1d7e66a7a88c6e3986e
Haider Ali Punjabi <haiderali@cyberservices.com>
2017-11-28 20:19:31 +0530

    Added Travis

---
+++ .travis/json.py
@@ -0,0 +1,34 @@
+import urllib3.request, urllib.request
+import requests as req
+import os
+
+def user_repos():
+    url = "https://api.github.com/users/haideralipunjabi/repos"
+    response = req.get(url, auth=(os.environ.get('GITHUB_USERNAME'), os.environ.get('GH_TOKEN')))
+    print(response.json(),file=open('user_repos.json',"a"))
+
+def closed_projects():
+    url = "https://hackesta.org/api/projects/?format=json"
+    response = req.get(url)
+    print(response.json(),file=open('closed_projects.json',"a"))
+
+def instagram():
+    url = "https://instagram.com/haideralipunjabi/?__a=1"
+    response = req.get(url)
+    print(response.json(),file=open('instagram.json',"a"))
+
+def hackesta_projects():
+    url = "https://api.github.com/orgs/hackesta/repos"
+    response = req.get(url, auth=(os.environ.get('GITHUB_USERNAME'), os.environ.get('GH_TOKEN')))
+    print(response.json(),file=open('hackesta_repos.json',"a"))
+
+def websites():
+    url = "https://hackesta.org/api/websites/?format=json"
+    response = req.get(url)
+    print(response.json(),file=open('websites.json',"a")
+
+user_repos()
+closed_projects()
+instagram()
+hackesta_projects()
+websites()
---
+++ .travis/push.sh
@@ -0,0 +1,20 @@
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add *.json
+  git commit --message "[ci skip] Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote remove origin
+  git remote add origin https://${GH_TOKEN}@github.com/hackesta/hackesta.org.git
+  git push origin HEAD:master
+}
+
+setup_git
+commit_website_files
+upload_files
