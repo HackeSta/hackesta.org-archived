fcbd85416730aa3bde3b4de1f8e7c0c47ea188ff
Haider Ali Punjabi <haiderali@cyberservices.com>
2017-11-28 21:12:28 +0530

    Update to Travis

--- .travis/get_data.py
+++ .travis/get_data.py
@@ -5,27 +5,28 @@ import json
 def user_repos():
     url = "https://api.github.com/users/haideralipunjabi/repos"
     response = req.get(url, auth=(os.environ.get('GITHUB_USERNAME'), os.environ.get('GH_TOKEN')))
-    print(json.dumps(response.text),file=open('data/user_repos.json',"w"))
+    print(response.text,file=open('data/user_repos.json',"w"))
 
 def closed_projects():
     url = "https://hackesta.org/api/projects/?format=json"
     response = req.get(url)
-    print(json.dumps(response.text),file=open('data/closed_projects.json',"w"))
+    print(response.text,file=open('data/closed_projects.json',"w"))
 
 def instagram():
     url = "https://instagram.com/haideralipunjabi/?__a=1"
     response = req.get(url)
-    print(json.dumps(response.text),file=open('data/instagram.json',"w"))
+    print(response.text,file=open('data/instagram.json',"w"))
 
 def hackesta_projects():
     url = "https://api.github.com/orgs/hackesta/repos"
     response = req.get(url, auth=(os.environ.get('GITHUB_USERNAME'), os.environ.get('GH_TOKEN')))
-    print(json.dumps(response.text),file=open('data/hackesta_repos.json',"w"))
+    print(response.text,file=open('data/hackesta_repos.json',"w"))
 
 def websites():
     url = "https://hackesta.org/api/websites/?format=json"
     response = req.get(url)
-    print(json.dumps(response.text),file=open('data/websites.json',"w"))
+    print(response.text,file=open('data/websites.json',"w"))
+
 user_repos()
 closed_projects()
 instagram()
